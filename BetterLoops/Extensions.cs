d3bbfa0709b1a6c0144fccb3e44fc68c03fe23dc
PawelMichniewicz <48495023+PawelMichniewicz@users.noreply.github.com>
2023-01-04 23:56:31 +0100

    cool foreach with Kotlin-like range

---
+++ BetterLoops/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace BetterLoops
+{
+    internal static class Extensions
+    {
+        internal static CustomIntEnumerator GetEnumerator(this Range range)
+        {
+            return new CustomIntEnumerator(range);
+        }
+
+        internal ref struct CustomIntEnumerator
+        {
+            private readonly int end;
+            private int current;
+
+            public CustomIntEnumerator(Range range)
+            {
+                if (range.End.IsFromEnd)
+                {
+                    throw new NotSupportedException();
+                }
+
+                current = range.Start.Value - 1;
+                end = range.End.Value;
+            }
+
+            public int Current => current;
+
+            public bool MoveNext()
+            {
+                current++;
+                return current <= end;
+            }
+        }
+    }
+}
--- BetterLoops/Program.cs
+++ BetterLoops/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // boring and bad performance
+            //foreach (var item in Enumerable.Range(1,10))
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // cool and fancy like Kotlin
+            foreach (var item in 5..13)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
